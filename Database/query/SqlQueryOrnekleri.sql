d32662dd8f62ef79c5c1d36db59d30a25dbef603
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-09 13:57:05 +0300

    Update SqlQueryOrnekleri.sql

--- Database/query/SqlQueryOrnekleri.sql
+++ Database/query/SqlQueryOrnekleri.sql
@@ -96,16 +96,110 @@
 --Almanya fransa ve ispanyad dýþýnda olan müþterilerin verilerini listeleme..  ( not in ) komutu çoklu or kullanýmýný kolaylaþtýran bir komut.
 --select CompanyName,City,Country from Customers where country not in ('Germany','France','Spain') 
 
---SubQuery  Alt Sorgu
+--					SubQuery  Alt Sorgu				--
 
 --Tedarikçilerimin olduðu ülkelerdeki müþtreliremiz kimler  ?   // Mesela benim kendim eklemiþ olduðum KBIL þirketi türkiyede yer alýyor ve o þirket Suppliers'da yer almadýðý için gelmedi..
 --Select * From Customers where Country in ( Select Country from Suppliers)
 
---Ürünler(Products) tablosunda fiyat bilgisi 100 ila 200 arasýnda olan ama ürün kategorisi 1,2,3 deðerlerinin dýþýnda olan kayýtlarý getiriniz...
---Select * from Products where (UnitPrice between 100 and 200 ) and CategoryID not in (1,2,3)
+-- Ürünler tablosundan fiyat bilgisi 100-200 arasýnda olan ama ürün kategorisi 1,2,3  dýþýnda olan 
+
+--select * from Products where (UnitPrice between 100 and 200)  (select CategoryID from Products where CategoryID not in (1,2,3))
+
+--						LIKE KOMUTU					--
+
+--Select * From Customers where CompanyName like 'a%'  -- Baþ harfi a ile baþlayan 
+--Select * From Customers where CompanyName like '_a%'  -- Ýkinci harfi a ile baþlayan
+--Select * From Customers where CompanyName like '%or%'  -- Ýçerisinde or bulunduran
+--Select * From Customers where CompanyName like '%a'  -- Sonu a ile biten
+--Select * From Customers where CompanyName like '%a_'  -- Sondan ikinci harfi a ile biten
+
+-- Örnek : CustomerID alanýnda baþ harfi c ile baþlayan müþteriler.. 
+--Select * From Customers where CustomerID like 'C%'
+
+-- Örnek : ContactName alanýnda mas karakteri olan müþteriler..
+--Select * From Customers where ContactName like '%mas%'
+
+-- Örnek : CompanyName alanýnda ikinci harfi r olan þirketler..
+--Select * From Customers where CompanyName like '_r%'
+
+-- Örnek : CompanyName alanýnda a ile baþlayan ve en az üç karakter uzunlugundaki þirketler..
+--Select * from Customers where CompanyName like 'a___%'
+
+-- Örnek : ContactName alanýnda a ile baþlayýp o ile biten þirketler..
+--Select * From Customers where ContactName like 'a%o'
+
+
+--						UNION ALL KOMUTU					--
+
+-- Örnek : Müþterilerin ve Tekdarikçilerin olduðu þehirleri tek bir tabloda listelemek..
+--Select City From Customers UNION ALL Select City From Suppliers order by city
 
 
 
+--						INSERT INTO KOMUTU					--
+--Insert into Customers (CustomerId,CompanyName,ContactName,Address,City,PostalCode,Country) values ('VGS','Vatan Gaming Store','Muhammet Sefer Bayram','Fevzi Çakmak Mah. No:122/4 Pendik','Istanbul',34890,'Turkey')
+--Update Customers set ContactTitle='Owner',Region='TR' where CustomerID='VGS'
+--Select * From Customers where City='Istanbul'
+
+-- NOT : Eðer herhangi bir þekilde kolon isimleri belirtilmemiþse mutlaka values deðerleri tablodaki kolon sayýsý kadar olmalýdýr..
+
+--Insert into Customers values ('FB','Fenerbahçe','Aziz Yýldýrým','Owner','Kadýkoy Sukru Saracoglu Stadyumu','Istanbul','TR',34788,'Turkey','555-555-1907','555-555-1907')
+--Select * From Customers where City='Istanbul' 
 
 
+--						DELETE KOMUTU					--
+--Delete from Customers where CustomerID='FB'
+--Select * From Customers where City='Istanbul'
 
+
+--						INNER JOIN KOMUTU					--
+-- Örnek : Orders Tablosundan varolan sipariþin müþterileri kimlermiþ öðrenelim  ..
+--Select OrderId,Orders.CustomerID,Customers.CompanyName,Customers.Phone from Orders Inner join Customers ON orders.CustomerID=Customers.CustomerID
+--Select *,Shippers.CompanyName From Orders Inner Join Shippers On orders.ShipVia=Shippers.ShipperID 
+
+-- Örnek : Orders tablosundan varolan sipariþlerin müþterileri adlarý ve hangi taþýmacý taþýyor ? 
+--Select Orders.CustomerID,ShipName,Shippers.CompanyName as 'Tasima Sirketi' ,Shippers.Phone as 'Telefon' , (Employees.FirstName +' '+Employees.LastName) as 'Satýcý' from Orders Inner Join Shippers on Orders.ShipVia=Shippers.ShipperID  Inner Join Employees On Orders.EmployeeID=Employees.EmployeeID
+
+
+-- Örnek : Çýkýþ Tablosu : Products , 
+--Ýstenen : Ýlgili ürünümü (Chai) acaba hangi tedarikçi (adýyla) (Tablo:Suppliers) saðlamaktadýr. Ve ürünümün kategorisi nedir adý/tanýmý)
+
+--Select ProductID,ProductName,UnitPrice,UnitsInStock,Suppliers.CompanyName as 'Satýcý Tedarikci' , Categories.CategoryName as ' Ürün Kategorisi' from Products  inner join Suppliers on Products.SupplierID=Suppliers.SupplierID
+--inner join Categories on Products.CategoryID=Categories.CategoryID
+
+
+-- Ýstenen : Sipariþler tablosundan hareketle , o sipariþin hangi tür kategoride olduðunu listeleyiniz.. Ýlgili tablolar : OrderDetails, Products ve Categories
+
+--Select OrderID,Quantity,Products.ProductName,Categories.CategoryName from [Order Details] inner join Products on Products.ProductID=[Order Details].ProductID
+--inner join Categories on Categories.CategoryID=Products.CategoryID
+
+
+--						SELF JOIN KOMUTU					--  Kendi üzerinde Join   
+
+--Select (E1.TitleOfCourtesy + E1.FirstName + ' ' + E1.LastName) as EmpName,E1.Title, (E2.TitleOfCourtesy + E2.FirstName + ' ' + E2.LastName) as ReportsToWho,E2.Title from Employees E1, Employees E2 where E1.ReportsTo = E2.EmployeeID order by E1.ReportsTo
+
+
+--						LEN FONKSÝYONU					--
+
+--Select ProductName,Len(ProductName) as 'Uzunluk' from Products    -- Ürün adlarýnýn uzulugunu getir ( bosluklar dahil sayar )
+--Select ProductName as 'Uzunluk' from Products  where Len(ProductName)<10 order by Len(ProductName) desc -- Ürün adlarý karakter sayýsý 10 dan küçük olanlarý listeler..
+
+
+--						SubString FONKSÝYONU					--
+
+--Select ProductName , SUBSTRING(ProductName,2,5) as 'SONUC' From Products   -- Ürün adýnýnýn 2. karakterinden itibaren 5 karakteri al ..
+
+
+--						LEFT/RIGHT FONKSÝYONU					--
+
+--Select ProductName , LEFT(ProductName,5) as 'SONUC' From Products -- Ürün adýnýn solundan 5 karakter al..
+--Select ProductName , RIGHT(ProductName,6) as 'SONUC' From Products  --  Ürün adýnýn saðýndan 6 karakter al..
+
+--						UPPER/LOWER FONKSÝYONU					--
+
+--Select ProductName , UPPER(ProductName) as 'SONUC' From Products  -- Ürün Adýný Komple Büyük Harf'e çevirir..
+--Select ProductName , LOWER(ProductName) as 'SONUC' From Products  -- Ürün Adýný Komple Küçük Harf'e çevirir..
+
+-- Örnek : ProductName'in sadece ilk karakterini küçük yap..
+
+Select ProductName,substring(ProductName,1,1) + Lower(SUBSTRING(ProductName,2,50)) from Products 
