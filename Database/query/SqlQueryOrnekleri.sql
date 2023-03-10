1610d45c64bff1403ac5b008b6163166d73a7f73
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-10 14:35:06 +0300

    Update SqlQueryOrnekleri.sql

--- Database/query/SqlQueryOrnekleri.sql
+++ Database/query/SqlQueryOrnekleri.sql
@@ -354,18 +354,23 @@
 --inner join Territories t on et.TerritoryID=t.TerritoryID inner join Region r on t.RegionID=r.RegionID where e.EmployeeID=3
 
 
---Create function funcBolgeGetir(@calisanID int)
---returns int 
+--Create function funcBolgeGetir6(@calisanID int)
+--returns varchar(50)
 --as begin
---declare @BolgeBilgi int
---Select @BolgeBilgi = r.RegionDescription from Employees e inner join EmployeeTerritories et on e.EmployeeID=et.EmployeeID 
---inner join Territories t on et.TerritoryID=t.TerritoryID inner join Region r on t.RegionID=r.RegionID where e.EmployeeID=3
+--declare @BolgeBilgi varchar(50)
+--Select @BolgeBilgi =r.RegionDescription from Employees e 
+--inner join EmployeeTerritories et on e.EmployeeID=et.EmployeeID 
+--inner join Territories t on et.TerritoryID=t.TerritoryID 
+--inner join Region r on t.RegionID=r.RegionID 
+--where e.EmployeeID=@calisanID 
+--group by e.EmployeeID 
 --return @BolgeBilgi
 --end
 
 
 
-Select EmployeeID,FirstName,LastName,dbo.funcBolgeGetir6(3) as 'Bölge' from Employees
+
+Select EmployeeID,FirstName,LastName,dbo.funcBolgeGetir6(3) as 'Bölge' from Employees where EmployeeID=3
 
 
 
